bd9b045bf2135048d5e68ecdd23c4663f6c2feb6
cobbdzon <cobb@cobbdzon.cc>
2026-07-13 22:28:06 +0800

    initial index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+import * as queries from "./db/queries";
+
+const app = new Hono();
+
+app.get("/hello", async (c) => {
+  return c.html(
+    <>Hello World!</>
+  )
+})
+
+app.post("/register", async (c) => {
+  const body = await c.req.json();
+  const { username, password } = body;
+})
+
+export default app;
